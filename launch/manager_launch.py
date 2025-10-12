6e830f95d9c94d72a72ba342b88e32ef85db4d8a
William Engel <william.engel@mdynamix.de>
2025-10-12 21:56:40 +0200

    Remove Launch Args

--- launch/manager_launch.py
+++ launch/manager_launch.py
@@ -1,23 +1,12 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'topic',
-            default_value='/rc/ackermann_cmd',
-            description='Topic to publish AckermannDriveStamped commands to.'
-        ),
-
         Node(
             package='drive_manager',
-            executable='drive_manager',
-            name='drive_manager_node',
+            executable='drive_manager_node',
+            name='drive_manager',
             output='screen',
-            remappings=[
-                ('/rc/ackermann_cmd', LaunchConfiguration('topic'))
-            ]
         )
     ])
